162322ed010c78c233e4160fb79d603c3f284ca8
RaulVigil <raul_eduardo25@hotmail.com>
2025-12-13 20:50:00 -0600

    Actualizar mensaje de la página de perfil a "Perfil en desarrollo okii"

--- src/Pages/Perfil/Perfil.jsx
+++ src/Pages/Perfil/Perfil.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import Api from "../../Services/api";
 
 export default function Perfil() {
-  return <div>Perfil Page</div>;
+  return <div>Perfil en desarrollo okii</div>;
 }
