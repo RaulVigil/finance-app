3618eae840cc7c9a857a68f0aefbe07ef75a2a6a
RaulVigil <raul_eduardo25@hotmail.com>
2025-12-19 08:20:50 -0600

    Actualizar configuración de Vite y agregar componente Registro

---
+++ src/Pages/Registro/Registro.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+import { useState } from "react";
+import { InputComponent } from "../../Components/InputComponent";
+
+import Api from "../../Services/api";
+
+import { useNavigate } from "react-router-dom";
+
+export default function Registro() {
+
+
+  return <></>;
+}
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-    // base: '/tripiazone/',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://financeapi.raulo.dev",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
