f0db8d74c452f008a8bff31935f1a21e10154b4e
RaulVigil <raul_eduardo25@hotmail.com>
2025-12-21 17:35:01 -0600

    Eliminar configuración de proxy en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://financeapi.raulo.dev",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
 });
